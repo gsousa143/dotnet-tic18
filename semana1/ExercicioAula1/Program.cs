0dbc3069ebd5de296e26276a55deeb7aed7b3e38
gabriel sousa <gabrielsousa442@gmail.com>
2023-11-08 12:37:33 -0300

    Adicionando os exercicios da Aula 1

---
+++ semana1/ExercicioAula1/Program.cs
@@ -0,0 +1,12 @@
+﻿#region tipos de dados
+int _int;
+//double _double;
+//bool _bool;
+string _string="100";
+
+_int = int.MaxValue;
+Console.WriteLine(_int);
+_int = int.Parse(_string);
+Console.WriteLine(_int);
+#endregion
+
