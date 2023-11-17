a570f5d5978e1fbbd6781c36a10b7768cd0a8aca
Gabriel <gabrielsousa4242@gmail.com>
2023-11-17 17:51:20 -0300

    resolucao 04

---
+++ 004/Program.cs
@@ -0,0 +1,20 @@
+﻿int x = 10;
+int y = 3;
+
+int soma = x+y;
+int sub = x-y;
+int mult = x*y;
+int div = x/y;
+
+Console.Write("O resultado da soma e: ");
+Console.WriteLine(soma);
+
+Console.Write("O resultado da subtracao e: ");
+Console.WriteLine(sub);
+
+Console.Write("O resultado da multiplicacao e: ");
+Console.WriteLine(mult);
+
+Console.Write("O resultado da divisao e: ");
+Console.WriteLine(div);
+
