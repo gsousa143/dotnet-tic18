eff0bb6c92c062106a21a0aa270f6e4fe0e0e504
Gabriel <gabrielsousa4242@gmail.com>
2023-11-17 17:23:33 -0300

    resolucao 03

---
+++ 003/Program.cs
@@ -0,0 +1,9 @@
+﻿
+/*A conversão entre double e int pode ser realizada por um cast direto, 
+de modo que a parte fracionaria será truncada e aproximada para zero
+*/
+double Double = 13.94446;
+int Int = (int)Double;
+
+Console.WriteLine(Double);
+Console.WriteLine(Int);
