703174687750102cf936a3fe074ca2d05fe3f867
gabriel <gabrielsousa4242@gmail.com>
2023-11-29 10:10:20 -0300

    Adicionando exercicios da aula 8

---
+++ semana4/exemplo4/Program.cs
@@ -0,0 +1,30 @@
+﻿Aluno a = new Aluno("gabriel",18);
+Console.WriteLine($"{a.Nome}, {a.Idade}");
+
+
+class Aluno{
+    private string nome;
+    private int idade;
+
+    public string Nome{
+        get{
+            return nome;
+        }
+        set{
+            nome = value;
+        }
+    }
+
+    public int Idade{
+        get{
+            return idade;
+        }
+        set{
+            idade = value;
+        }
+    }
+    public Aluno(string nome,int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
