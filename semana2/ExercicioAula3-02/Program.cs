6d0b359a0724ec3ee934409745cdefe0535d1058
gabriel <gabrielsousa4242@gmail.com>
2023-11-29 10:14:42 -0300

    commit inicial

--- semana2/ExercicioAula3-02/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿int fib1 = 1, fib2 = 1, fib=0;
-do{
-    fib = fib1+fib2;
-    fib1 = fib2;
-    fib2 = fib;
-    Console.WriteLine(fib1);
-}while(fib<100);
--- semana2/ExercicioAula3-03/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿for (int n = 1;n<8+1;n++){
-    for(int i = 1;i<n+1;i++){
-        Console.Write($"{i*n}\t");
-    }
-    Console.WriteLine();
-}
